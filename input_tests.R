d8e7065359e6b3760f91ee1dc6765ef931f6a53b
jejoenje <jeroenminderman@gmail.com>
2022-02-07 21:06:09 +0000

    Input tests working, apparently. Had to switch to custom input field setup and revised means to operate and complete dropdown boxes

--- input_tests.R
+++ input_tests.R
@@ -5,7 +5,7 @@ library(rvest)
 
 source("AutoDemOn_functions.R")
 rD <- rsDriver(
-  port = 4480L,
+  port = 4484L,
   browser = c("firefox"),
   version = "latest",
 )
@@ -41,13 +41,14 @@ remDr$navigate("https://app.bto.org/demography/bto/main/data-entry/list-style/li
 ifield <- remDr$findElements("id", "settingsButton")
 ifield[[1]]$clickElement()
 ifield <- remDr$findElements("name", "field-setup-list")
-ifield[[1]]$sendKeysToElement(list("All base Capture Fields Setup for Export"))
-#ifield[[1]]$sendKeysToElement(list("CRING RESIGHTINGS GULLS"))
+#ifield[[1]]$sendKeysToElement(list("All base Capture Fields Setup for Export"))
+ifield[[1]]$sendKeysToElement(list("CRING RESIGHTINGS GULLS"))
+#ifield <- remDr$findElements("id", "settingsButton")
 ifield <- remDr$findElements("id", "settingsButton")
 ifield[[1]]$clickElement()
 
 ###### ITERATE DATA ENTRY:
-i <- 2
+i <- 3
 
 dd_i <- dd[i,]
 
@@ -60,57 +61,37 @@ ifield <- remDr$findElements("name", "ring_no")
 ifield[[1]]$sendKeysToElement(list(dd_i[,"Metal.Ring"], key="tab"))
 # Visit date:
 ifield <- remDr$findElements("name", "visit_date")
-ifield[[1]]$sendKeysToElement(list(format(dd_i[,"Date"],"%d/%m/%y"), key="tab"))
+ifield[[1]]$sendKeysToElement(list(format(dd_i[,"Date"],"%d/%m/%y"), key="enter"))
+ifield[[1]]$click()
+
+# ifield <- remDr$findElements("class name", "chosenFieldSetup")
+# ifield[[11]]$clickElement()
 
 # Location:
 ### Tab to it
-# ifield <- remDr$findElements("name", "capture_time")
-# ifield[[1]]$sendKeysToElement(list("", key="tab"))
-ifield[[1]] <- remDr$findElements("id", "s2id_autogen7")
-ifield[[1]][[1]]$clickElement()
+#ifield <- remDr$findElements("name", "capture_time")
+#ifield[[1]]$sendKeysToElement(list("", key="tab"))
+#ifield <- remDr$findElements("id", "s2id_autogen7")
 
-### THIS WORKS WHEN DROPDOWN IS "DROPPED":
+ifield <- remDr$findElements("class name", "select2-arrow")
+ifield[[3]]$clickElement()
 ifield <- remDr$findElements("id", "s2id_autogen8_search")
-ifield[[1]]$sendKeysToElement(list(dd[i,"SiteCode"], key = "tab"))
-
+ifield[[1]]$sendKeysToElement(list(dd[i,"SiteCode"], key = "enter"))
 
 # Finding circumstances
 ifield <- remDr$findElement("name", "finding_circumstances")
-#ifield$clickElement()
-ifield$sendKeysToElement(list("81 Identified By Colour Rings(s)"))
-
-
-
-ifield <- remDr$findElement("name", "finding_circumstances")
-
-remDr$findElement("xpath", "//span[@class='text'][contains(text(),'81 Identified By Colour Ring(s)')]/parent::a")
-
-
-ifield <- remDr$findElement("name", "finding_circumstances")
 ifield$clickElement()
-remDr$findElement(using = 'xpath', value = "//option[contains(text(),'81 Identified By Colour Ring(s)')]")
-
-
-ifield$sendKeysToElement(list("--", key = "tab"))
-
-
-
+ifield <- remDr$findElement(using = 'xpath', value = "//*[@value='81']")
+ifield$clickElement()
 
 ### Left Leg Below
 ifield <- remDr$findElements("name", "left_leg_below")
 ifield[[1]]$sendKeysToElement(list(paste0("YN(",dd[i,"Code"],")"), key = "tab"))
 ### Right Leg Below
 ifield <- remDr$findElements("name", "right_leg_below")
+ifield[[1]]$sendKeysToElement(list("M", key = "tab"))
 
 ### Finder name
 ifield <- remDr$findElements("name", "finder_name")
 ifield[[1]]$sendKeysToElement(list(dd[i,"Observer/Notes"], key = "tab"))
-
-### Save
-#ifield <- remDr$findElements("name", "toe_span")
-#ifield[[1]]$sendKeysToElement(list("", key = "tab"))
-
-ifield <- remDr$findElement(using = "xpath", value = "//button[@class='saveNewRecord savingRecordBtn btn btn-primary']")
-ifield$clickElement()
-
 
